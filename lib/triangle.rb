c8df01eecf24643ee83a92f2927c7a0a3eec3c3b
phylis13 <phylisnjeri54@gmail.com>
2023-02-17 13:21:21 +0300

    done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
 class Triangle
-  # write code here
+  attr_accessor :first, :second, :third
+  
+  def initialize(side1, side2, side3)
+    @first = side1
+    @second = side2
+    @third = side3
+  end
+
+  def kind
+    xy = @first + @second
+    yz = @second + @third
+    xz = @first + @third
+
+    if (xy > @third && yz > @first && xz > @second && 
+      @first > 0 && @second > 0 && @third > 0)
+      if (@first == @second && @second == @third)
+        return :equilateral
+      elsif (@first == @second || @first == @third || @second == @third)
+        return :isosceles
+      else
+        return :scalene
+      end
+
+    else
+      begin
+        raise TriangleError
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Invalid triangle"
+    end
+  end
+
 end
